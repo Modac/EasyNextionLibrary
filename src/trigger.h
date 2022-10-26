3c3f5c913494827c473280444224bf2fd119579b
Modac <Modac@users.noreply.github.com>
2022-10-26 16:52:53 +0200

    Remove unnecessary declarations and add empty definition
    Prevents runtime fault when a function is called but not defined by the user.

--- src/trigger.h
+++ src/trigger.h
@@ -30,119 +30,62 @@
 
 // weak attribute funcion for read the custom command protocol
 
-extern void easyNexReadCustomCommand();
-extern void easyNexReadCustomCommand() __attribute__((weak));
+extern void easyNexReadCustomCommand() __attribute__((weak)) {}
 
-extern void toSleep();
-extern void wakeUp();
+extern void toSleep() __attribute__((weak)) {}
+extern void wakeUp() __attribute__((weak)) {}
 
-extern void toSleep() __attribute__((weak));
-extern void wakeUp() __attribute__((weak));
-
-// declare the functions for triggers
-extern void trigger0();
-extern void trigger1(); 
-extern void trigger2();
-extern void trigger3();
-extern void trigger4();
-extern void trigger5();
-extern void trigger6();
-extern void trigger7();
-extern void trigger8();
-extern void trigger9();
-extern void trigger10();
-extern void trigger11();
-extern void trigger12();
-extern void trigger13();
-extern void trigger14();
-extern void trigger15();
-extern void trigger16();
-extern void trigger17();
-extern void trigger18();
-extern void trigger19();
-extern void trigger20();
-extern void trigger21();
-extern void trigger22();
-extern void trigger23();
-extern void trigger24();
-extern void trigger25();
-extern void trigger26();
-extern void trigger27();
-extern void trigger28();
-extern void trigger29();
-extern void trigger30();
-extern void trigger31();
-extern void trigger32();
-extern void trigger33();
-extern void trigger34();
-extern void trigger35();
-extern void trigger36();
-extern void trigger37();
-extern void trigger38();
-extern void trigger39();
-extern void trigger40();
-extern void trigger41();
-extern void trigger42();
-extern void trigger43();
-extern void trigger44();
-extern void trigger45();
-extern void trigger46();
-extern void trigger47();
-extern void trigger48();
-extern void trigger49();
-extern void trigger50();
-
-// declaration of the function as weak
-extern void trigger0() __attribute__((weak));
-extern void trigger1() __attribute__((weak));
-extern void trigger2() __attribute__((weak));
-extern void trigger3() __attribute__((weak));
-extern void trigger4() __attribute__((weak));
-extern void trigger5() __attribute__((weak));
-extern void trigger6() __attribute__((weak));
-extern void trigger7() __attribute__((weak));
-extern void trigger8() __attribute__((weak));
-extern void trigger9() __attribute__((weak));
-extern void trigger10() __attribute__((weak));
-extern void trigger11() __attribute__((weak));
-extern void trigger12() __attribute__((weak));
-extern void trigger13() __attribute__((weak));
-extern void trigger14() __attribute__((weak));
-extern void trigger15() __attribute__((weak));
-extern void trigger16() __attribute__((weak));
-extern void trigger17() __attribute__((weak));
-extern void trigger18() __attribute__((weak));
-extern void trigger19() __attribute__((weak));
-extern void trigger20() __attribute__((weak));
-extern void trigger21() __attribute__((weak));
-extern void trigger22() __attribute__((weak));
-extern void trigger23() __attribute__((weak));
-extern void trigger24() __attribute__((weak));
-extern void trigger25() __attribute__((weak));
-extern void trigger26() __attribute__((weak));
-extern void trigger27() __attribute__((weak));
-extern void trigger28() __attribute__((weak));
-extern void trigger29() __attribute__((weak));
-extern void trigger30() __attribute__((weak));
-extern void trigger31() __attribute__((weak));
-extern void trigger32() __attribute__((weak));
-extern void trigger33() __attribute__((weak));
-extern void trigger34() __attribute__((weak));
-extern void trigger35() __attribute__((weak));
-extern void trigger36() __attribute__((weak));
-extern void trigger37() __attribute__((weak));
-extern void trigger38() __attribute__((weak));
-extern void trigger39() __attribute__((weak));
-extern void trigger40() __attribute__((weak));
-extern void trigger41() __attribute__((weak));
-extern void trigger42() __attribute__((weak));
-extern void trigger43() __attribute__((weak));
-extern void trigger44() __attribute__((weak));
-extern void trigger45() __attribute__((weak));
-extern void trigger46() __attribute__((weak));
-extern void trigger47() __attribute__((weak));
-extern void trigger48() __attribute__((weak));
-extern void trigger49() __attribute__((weak));
-extern void trigger50() __attribute__((weak));
+// declaration of the trigger functions as weak
+extern void trigger0() __attribute__((weak)) {}
+extern void trigger1() __attribute__((weak)) {}
+extern void trigger2() __attribute__((weak)) {}
+extern void trigger3() __attribute__((weak)) {}
+extern void trigger4() __attribute__((weak)) {}
+extern void trigger5() __attribute__((weak)) {}
+extern void trigger6() __attribute__((weak)) {}
+extern void trigger7() __attribute__((weak)) {}
+extern void trigger8() __attribute__((weak)) {}
+extern void trigger9() __attribute__((weak)) {}
+extern void trigger10() __attribute__((weak)) {}
+extern void trigger11() __attribute__((weak)) {}
+extern void trigger12() __attribute__((weak)) {}
+extern void trigger13() __attribute__((weak)) {}
+extern void trigger14() __attribute__((weak)) {}
+extern void trigger15() __attribute__((weak)) {}
+extern void trigger16() __attribute__((weak)) {}
+extern void trigger17() __attribute__((weak)) {}
+extern void trigger18() __attribute__((weak)) {}
+extern void trigger19() __attribute__((weak)) {}
+extern void trigger20() __attribute__((weak)) {}
+extern void trigger21() __attribute__((weak)) {}
+extern void trigger22() __attribute__((weak)) {}
+extern void trigger23() __attribute__((weak)) {}
+extern void trigger24() __attribute__((weak)) {}
+extern void trigger25() __attribute__((weak)) {}
+extern void trigger26() __attribute__((weak)) {}
+extern void trigger27() __attribute__((weak)) {}
+extern void trigger28() __attribute__((weak)) {}
+extern void trigger29() __attribute__((weak)) {}
+extern void trigger30() __attribute__((weak)) {}
+extern void trigger31() __attribute__((weak)) {}
+extern void trigger32() __attribute__((weak)) {}
+extern void trigger33() __attribute__((weak)) {}
+extern void trigger34() __attribute__((weak)) {}
+extern void trigger35() __attribute__((weak)) {}
+extern void trigger36() __attribute__((weak)) {}
+extern void trigger37() __attribute__((weak)) {}
+extern void trigger38() __attribute__((weak)) {}
+extern void trigger39() __attribute__((weak)) {}
+extern void trigger40() __attribute__((weak)) {}
+extern void trigger41() __attribute__((weak)) {}
+extern void trigger42() __attribute__((weak)) {}
+extern void trigger43() __attribute__((weak)) {}
+extern void trigger44() __attribute__((weak)) {}
+extern void trigger45() __attribute__((weak)) {}
+extern void trigger46() __attribute__((weak)) {}
+extern void trigger47() __attribute__((weak)) {}
+extern void trigger48() __attribute__((weak)) {}
+extern void trigger49() __attribute__((weak)) {}
+extern void trigger50() __attribute__((weak)) {}
 
 #endif
